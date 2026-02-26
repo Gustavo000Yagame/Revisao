27b00039a91c3497923252a2f53a9bc979850e08
🔐 Yagame <geraisusos25@gmail.com>
2026-02-25 22:15:39 -0300

    Add files via upload

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -7,3 +7,14 @@ function calcular(){
     const numero2 = parseFloat(document.getElementById('numero2').value)
     const operacao = document.getElementById('operacao').value
 }
+switch(operacao){
+    case 'sumar':
+        soma = numero1 + numero2
+        document.getElementById('resultado').textContent = `Resultado: ${soma}`
+        break
+    case 'diminuir':
+        subtracao = numero1 - numero2
+        document.getElementById('resultado').textContent = `Resultado: ${subtracao}`
+        break
+
+}
