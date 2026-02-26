5f0e2c600f12a1257a349c0478276155a4adca95
🔐 Yagame <geraisusos25@gmail.com>
2026-02-25 22:08:11 -0300

    Add files via upload

---
+++ Calculadora/calculadora.js
@@ -0,0 +1,9 @@
+let soma
+let subtracao   
+let multiplicacao
+let divisao
+function calcular(){
+    const numero1 = parseFloat(document.getElementById('numero1').value)
+    const numero2 = parseFloat(document.getElementById('numero2').value)
+    const operacao = document.getElementById('operacao').value
+}
