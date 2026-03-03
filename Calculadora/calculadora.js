71fee63e06e42c4708494f78d2aba8d4e3387d57
🔐 Yagame <geraisusos25@gmail.com>
2026-03-02 22:12:52 -0300

    Add files via upload

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -2,19 +2,35 @@ let soma
 let subtracao   
 let multiplicacao
 let divisao
-function calcular(){
-    const numero1 = parseFloat(document.getElementById('numero1').value)
-    const numero2 = parseFloat(document.getElementById('numero2').value)
-    const operacao = document.getElementById('operacao').value
-}
-switch(operacao){
-    case 'sumar':
-        soma = numero1 + numero2
-        document.getElementById('resultado').textContent = `Resultado: ${soma}`
-        break
-    case 'diminuir':
-        subtracao = numero1 - numero2
-        document.getElementById('resultado').textContent = `Resultado: ${subtracao}`
-        break
 
+function calcular(event){
+    event.preventDefault()
+    const numero1 = parseFloat(document.getElementById('num1').value)
+    const numero2 = parseFloat(document.getElementById('num2').value)
+    const operacao = document.getElementById('operacao').value
+    
+    switch(operacao){
+        case 'somar':
+            soma = numero1 + numero2
+            document.getElementById('resultado').textContent = `Resultado: ${soma}`
+            break
+        case 'diminuir':
+            subtracao = numero1 - numero2
+            document.getElementById('resultado').textContent = `Resultado: ${subtracao}`
+            break
+        case 'multiplicar':
+            multiplicacao = numero1 * numero2
+            document.getElementById('resultado').textContent = `Resultado: ${multiplicacao}`
+            break
+        case 'dividir':
+            if (numero2 !== 0) {
+                divisao = numero1 / numero2
+                document.getElementById('resultado').textContent = `Resultado: ${divisao}`
+            } else {
+                document.getElementById('resultado').textContent = 'Erro: Divisao por zero nao e permitida.'
+            }
+            break
+    }
 }
+
+document.getElementById('calculadora').addEventListener('submit', calcular)
