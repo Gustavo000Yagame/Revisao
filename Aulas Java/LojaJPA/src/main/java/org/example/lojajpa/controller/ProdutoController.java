331e9dfc00b286a11ae6ef8ef9829604bc3f1241
🔐 Yagame <geraisusos25@gmail.com>
2026-03-11 21:43:24 -0300

    Add files via upload

---
+++ Aulas Java/LojaJPA/src/main/java/org/example/lojajpa/controller/ProdutoController.java
@@ -0,0 +1,21 @@
+package org.example.lojajpa.controller;
+
+import org.example.lojajpa.service.ProdutoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoService service;
+
+    public ProdutoController(ProdutoService service){
+            this.service = service;
+
+    }
+
+        @GetMapping
+
+}
