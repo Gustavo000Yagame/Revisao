d2367fbac8967e371cfe8a6d0a0d3f0674ad2a96
🔐 Yagame <geraisusos25@gmail.com>
2026-03-11 21:07:41 -0300

    Add files via upload

---
+++ Aulas Java/LojaJPA/src/main/java/org/example/lojajpa/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package org.example.lojajpa.repository;
+
+import org.example.lojajpa.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long > {
+
+}
